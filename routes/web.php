28227e3eb0cf40075a8e8d8ab414c1f430db6f0c
vegaliu <vegaliu@foxmail.com>
2022-09-23 03:18:19 +0000

    样式美化

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
