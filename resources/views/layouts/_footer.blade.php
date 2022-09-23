8bece4bb827df20e016bdd728df64e6e32d6757c
vegaliu <vegaliu@foxmail.com>
2022-09-23 12:39:08 +0000

    布局中的链接-命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="footer">
-  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/images/201901/24/1/OyBnfB2vlk.png!/both/44x44">
   <a href="https://learnku.com/laravel/courses" target=_blank class="text-decoration-none">
     刻意练习，每日精进
   </a>
 
   <div class="float-end">
-    <a href="/about" class="text-decoration-none">关于</a>
+    <a href="{{ route('about') }}" class="text-decoration-none">关于</a>
   </div>
 </footer>
