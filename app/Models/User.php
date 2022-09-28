c0d680079eaf5b78b6d174e41233eea599373b31
vegaliu <vegaliu@foxmail.com>
2022-09-28 09:06:13 +0000

    添加头像

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,6 +40,6 @@ class User extends Authenticatable
     public function gravatar($size='100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "https://cdn.v2ex.com/gravatar/$hash?s=$size";
+        return "https://cdn.sep.cc/avatar/$hash?s=$size";
     }
 }
